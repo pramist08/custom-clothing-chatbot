9d07f6163160f66d8463ebbc473e5ab75b4d1f44
pramist08 <pramist08@gmail.com>
2025-11-02 13:47:29 +0530

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/config.php';
 
 // ---------- Logging Setup ----------
-define('LOG_FILE', __DIR__ . '/bot.log');
+define('LOG_FILE', __DIR__ . '/bot1.log');
 
 /**
  * Write a log entry with a timestamp and optional data.
